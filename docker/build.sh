cab2792dcd6e186af4142cf65a3ae91f7ea74f15
Aaron Son <aaron@dolthub.com>
2025-07-28 16:55:09 -0700

    docker/Dockerfile: Bump some dependencies. Rebuild to pick up amazonlinux bumps.

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,4 @@ set -e
 set -o pipefail
 
 (cd ../server && GOOS=linux GOARCH=arm64 go build -o ../docker/server .)
-docker build -t 407903926827.dkr.ecr.us-west-2.amazonaws.com/dolt_lambda_bats_runner .
+docker build --platform=linux/arm64 -t 407903926827.dkr.ecr.us-west-2.amazonaws.com/dolt_lambda_bats_runner .
